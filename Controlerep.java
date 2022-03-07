54133a98a29102c596e6bb36476ecaa9871f3308
Gustavo Cassemiro Silva <gustavo-santosfc@hotmail.com>
2022-03-07 19:40:21 +0000

    Salvando controlerep

---
+++ Controlerep.java
@@ -0,0 +1,18 @@
+public class Controlerep {
+
+    public static void main(String[] args) {
+
+        int x = 19;
+
+        do {
+
+            System.out.println("Vc não tem permissão para entrar");
+            x++;
+
+            
+            
+
+        }
+        while (x <18);
+    }
+}
